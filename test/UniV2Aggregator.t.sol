82c0603f62b30e666d39f1f65c7ea9c162d5bf9c
eugenioclrc <eugenioclrc@gmail.com>
2025-08-16 00:33:34 -0300

    hook will only use swap

--- test/UniV2Aggregator.t.sol
+++ test/UniV2Aggregator.t.sol
@@ -58,8 +58,7 @@ contract CounterTest is Test, Deployers {
         // Deploy the hook to an address with the correct flags
         address flags = address(
             uint160(
-                Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
-                    | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+                Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG
             ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
         );
         bytes memory constructorArgs = abi.encode(poolManager); // Add all the necessary constructor arguments from the hook
